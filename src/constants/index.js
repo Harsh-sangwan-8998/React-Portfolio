54b88ea320867500a4c240305b0438af2628e756
Harsh-8998 <harshsangwan8998@gmail.com>
2024-11-01 22:41:01 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,71 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpeg";
+
+export const HERO_CONTENT = `I am a Cyber Security student and a software developer skilled in the React and Django frameworks, with a solid foundation in Node.js and Express.js. With hands-on experience in database management using MongoDB and SQL, I bring a cybersecurity perspective to my development projects, focusing on creating secure, threat-aware websites. My expertise in network monitoring also strengthens my ability to build and maintain robust, secure applications.`;
+
+export const ABOUT_TEXT = `I'm a dedicated software developer with expertise across several modern frameworks and technologies. My strong command of React and Django has enabled me to design and implement dynamic, responsive, and user-centered applications. Additionally, my knowledge of Node.js and Express.js allows me to create scalable backend services that efficiently manage server and client interactions, enabling smooth data flow and fast response times. Beyond front-end and back-end development, I bring hands-on experience with databases like MongoDB and SQL. With a background in cybersecurity, I have developed an acute awareness of web application threats, including injection attacks, cross-site scripting (XSS), and data breaches. My knowledge in this area empowers me to take proactive measures in building secure applications, employing best practices like encryption, secure authentication, and vulnerability testing to safeguard user data and maintain application integrity.`;
+
+export const EXPERIENCES = [
+  {
+    year: "1 Sept 2024 - 30 Sept 2024",
+    role: "Network Analyst",
+    company: "Code Alpha",
+    description: `As a Network Analyst intern, you would play a crucial role in monitoring and analyzing network traffic to ensure the security and efficiency of a network. Role as a Traffic Monitoring , Creating a Network Sniffer code , Presentation on Phising attack. `,
+    technologies: ["Python", "VS Code "],
+  },
+  {
+    year: "July 2023 - Aug 2023",
+    role: "Frontend Developer",
+    company: "Intern Pe",
+    description: `Designed and developed user interfaces for web applications using HTML, CSS and Javascript.`,
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    year: "June 2023 - July 2023",
+    role: "Group Leader",
+    company: "Sankalp Foundation",
+    description: `Inspired and mentored team members to actively participate in and contribute to the Foundation's activities through impactful knowledge sharing. Inspire People about Plantation.`,
+    technologies: [" "],
+  },
+ 
+];
+
+export const PROJECTS = [
+  {
+    title: "Secure Chat Application",
+    image: project1,
+    description:
+      "Developing  a secure chat application, implementing the AES algorithm to ensure robust data protection and encryption.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB", "AES", "Sliding Window Protcol"],
+  },
+ 
+  {
+    title: "Portfolio Website",
+    image: project2,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "TailwindCSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Word Counter Application",
+    image: project3,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Hospital Website",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Django", "Bootstrap","NoSQL"],
+  },
+];
+
+export const CONTACT = {
+  address: " Rohtak Harayana , INDIA ",
+  phoneNo: "+91 9306038998 ",
+  email: "harshsangwan8998@gmail.com",
+};
